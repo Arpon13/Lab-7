cf4f7e3abd6356deea786de4c72a4f907cddf57c
Arpon13 <arpon15-5655@diu.edu.bd>
2025-03-12 10:49:53 +0600

    Custom AppBar

--- lib/screens/home/page/home.dart
+++ lib/screens/home/page/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_9/utils/constants/colors.dart';
-import 'package:iconsax/iconsax.dart';
+
+import '../../../common/widgets/app_bar.dart';
+//import 'package:flutter_application_9/utils/constants/colors.dart';
+//import 'package:iconsax/iconsax.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -9,27 +11,8 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
 
-      appBar: AppBar(
-        title: Text("Mega Shop",
-        style: TextStyle(
-          fontFamily: "DMSans",
-          fontWeight: FontWeight.w700,
-          color: FColors.primaryNavyBlack,
-          ),
-        ),
-        centerTitle: true,
-        actions: [
-          Container(
-            padding: EdgeInsets.fromLTRB(5, 10, 10, 5),
-            child: Icon(Iconsax.notification_bing),
-            ),
-          Container(
-            padding: EdgeInsets.fromLTRB(5, 10, 10, 5),
-            child: Icon(Iconsax.shopping_cart),
-            ),
-        
-        ],
-      ),  
+      appBar: FAppBar(),  
     );
   }
 }
+
